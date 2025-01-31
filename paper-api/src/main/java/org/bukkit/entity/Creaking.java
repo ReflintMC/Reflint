14c213ed01d7d1c65654b72bc20a4ecf005e25fd
bUmmy1337 <92600850+bUmmy1337@users.noreply.github.com>
2025-01-31 16:01:10 +0300

    Experimental annotation changes

--- paper-api/src/main/java/org/bukkit/entity/Creaking.java
+++ paper-api/src/main/java/org/bukkit/entity/Creaking.java
@@ -1,11 +1,9 @@
 package org.bukkit.entity;
 
-import org.jetbrains.annotations.ApiStatus;
 
 /**
  * Represents a Creaking.
  */
-@ApiStatus.Experimental
 public interface Creaking extends Monster {
 
 }
